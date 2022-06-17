76c2ae45851b882a8ac19b9ea653bdaee31bbd5c
Francisco Rotundo <francorotundo90@gmail.com>
2022-06-17 00:55:06 -0400

    Se agrego en el settings la app polls, se crearon los primeros modelos, crearon los primeros views, se configuro el administrador de datos, y se creo el primer borrador del template

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,6 +1,20 @@
+from django.shortcuts import render
 from django.http import HttpResponse 
 
-def index(request):
-    return HttpResponse('Hello world')
+from .models import Question
 
-# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {"latest_question_list": latest_question_list})
+
+
+def detail(request, question_id):
+    return HttpResponse (f"Estas viendo la pregunta numero {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse (f"Estas viendo los resultados de la pregunta {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse (f"Estas votando a la pregunta {question_id}")
