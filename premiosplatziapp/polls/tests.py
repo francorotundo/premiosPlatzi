1200ee5709c94d26e0182102693c3d49da7905f2
Francisco Rotundo <francorotundo90@gmail.com>
2022-06-22 23:09:21 -0400

    Se crearon las vistas de details y results, tambien se implementaron test para el modelo Question y para la vista IndexView

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.urls import reverse
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_questions(self):
+        """was_published_recently returns False for questions whose pub_date is in the future"""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_quetions = Question(question_text="¿Quien es el mejor Course Director de Platzi?", pub_date = time)
+        self.assertIs(future_quetions.was_published_recently(), False)
+
+    def test_was_published_recently_with_past_questions(self):
+        """was_published_recently returns False for questions whose pub_date is in the past"""
+        time = timezone.now() - datetime.timedelta(days=2)
+        past_quetions = Question(question_text="¿Quien es el mejor Programador de Platzi?", pub_date = time)
+        self.assertIs(past_quetions.was_published_recently(), False)
+
+    def test_was_published_recently_with_recently_questions(self):
+        """was_published_recently returns True for questions whose pub_date less than a day"""
+        time = timezone.now() - datetime.timedelta(days=0.5)
+        recently_quetions = Question(question_text="¿Quien es el mejor Programador de Platzi?", pub_date = time)
+        self.assertIs(recently_quetions.was_published_recently(), True)
+
+
+class QuestionIndexViewTest(TestCase):
+    def test_no_questions(self):
+        """If no question exist, an appropiate message is displayed"""
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are availables")
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,20 +1,60 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
+from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect 
+from django.views import generic #esta nos permite instaciar nuestras vistas basadas en clases
+from django.utils import timezone
 
-from .models import Question
+from .models import Question, Choice
 
-def index(request):
-    latest_question_list = Question.objects.all()
-    return render(request, "polls/index.html", {"latest_question_list": latest_question_list})
-
-
-def detail(request, question_id):
-    return HttpResponse (f"Estas viendo la pregunta numero {question_id}")
+# def index(request):
+#     latest_question_list = Question.objects.all()
+#     return render(request, "polls/index.html", {
+#         "latest_question_list": latest_question_list
+#         })
 
 
-def results(request, question_id):
-    return HttpResponse (f"Estas viendo los resultados de la pregunta {question_id}")
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {
+#         "question": question
+#         })
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html",{
+#         "question": question
+#     })
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published question"""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5] #order_by se utiliza para ordenar se coloca un - para organizar de forma descendente y para traer cierta cantidad de resultados se utiliza los slices en este caso los primeros 5 valores generados por el order_by [:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(DetailView):
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
-    return HttpResponse (f"Estas votando a la pregunta {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"]) #request.POST["name en el input del formulario"] de esta forma traemos la información para que pueda ser utilizada .
+        
+    except (KeyError, Choice.DoesNotExist): #KeyError es para cuando el usuarion no escoge una opción y choise.DoesNotExist es para cuando la opción seleciona no exite.
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "No elegiste una respuesta"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,))) #aquí se redirecciona la pag a results organizando en orden descendente las diferentes opciones 
